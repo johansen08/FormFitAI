74fb9fa90b7dad55ea28b220c63cba12089dd3c6
zizmo <aziz.workplace28@gmail.com>
2024-06-20 23:49:25 +0700

    logo+avatar

--- Mobile Development/app/build.gradle.kts
+++ Mobile Development/app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+
     // Nav
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
